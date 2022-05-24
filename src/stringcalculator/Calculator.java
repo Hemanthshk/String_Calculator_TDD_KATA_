403be6a07b578ee17af1793325f4a914c3d7cdc8
Hemanth_SHK <hemanthkumar.s300@gmail.com>
2022-05-25 02:12:46 +0530

    Extracted Methods

--- src/stringcalculator/Calculator.java
+++ src/stringcalculator/Calculator.java
@@ -8,9 +8,17 @@ public class Calculator {
 
     public static int add(String numbers) {
         if(!numbers.isEmpty()){
-            List<Integer> num = Arrays.stream(numbers.split(",|\n")).map(Integer::parseInt).collect(Collectors.toList());
+            List<Integer> num = strArrToIntList(numbers);
             return num.stream().reduce(Integer::sum).orElseThrow(Error::new);
         }
         return 0;
+    }
+
+    private static List<Integer> strArrToIntList(String numbers) {
+        return Arrays.stream(getSplit(numbers)).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private static String[] getSplit(String numbers) {
+        return numbers.split(",|\n");
     }
 }
